c07a0dede273044c26f5041effcf11c2aaff36bc
Yizhen Liu <imliuyzh@outlook.com>
2021-08-29 18:52:58 -0700

    Set up the project

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import '@patternfly/react-core/dist/styles/base.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
