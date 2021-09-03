8ad65c2fbf84eb794ef32648a4d860e946903cbf
Yizhen Liu <imliuyzh@outlook.com>
2021-09-02 18:58:49 -0700

    Add node cache package

--- server/src/utils/instructorList.js
+++ server/src/utils/instructorList.js
@@ -1,14 +1,19 @@
+const NodeCache = require('node-cache');
 const { QueryTypes } = require('sequelize');
+
 const sequelize = require('../db/sequelize');
 const logger = require('./logger');
 
-const instructorList = [];
+const cache = new NodeCache();
 
 async function loadInstructors() {
-    if (instructorList.length === 0) {
+    let instructorList = cache.get('instructors');
+    if (instructorList === undefined) {
+        instructorList = [];
         logger.info(`Begin to Retrieve All the Instructors' Name...`);
         let instructors = await sequelize.query('SELECT DISTINCT name FROM Instructor', { type: QueryTypes.SELECT });
         instructors.forEach(instructor => instructorList.push(instructor.name));
+        cache.set('instructors', instructorList);
         logger.info(`Finished Retrieving All the Instructors' Name.`);
     }
     return instructorList;
