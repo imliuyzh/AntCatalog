8e3c4798fe819b91f41ed6f1f53562eff6142e13
Yizhen Liu <imliuyzh@outlook.com>
2021-12-22 02:15:00 -0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -17,11 +17,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet({ contentSecurityPolicy: false }));
 app.use(compression());
-//app.use(express.static(path.join(__dirname, '..', 'client', 'build')));   // Comment it out occurrences when doing local development
+//app.use(express.static(path.join(__dirname, '..', 'client', 'build')));   // Comment it out when doing local development
 
 app.use('/api/v1/search', rateLimiter, searchRouter);
 app.use('/complete/instructors', instructorAutocompleteRouter);
-//app.get('/', (_, res) => res.sendFile(path.resolve(`${__dirname}/../client/build/index.html`)));   // Comment it out occurrences when doing local development
+//app.get('/', (_, res) => res.sendFile(path.resolve(`${__dirname}/../client/build/index.html`)));   // Comment it out when doing local development
 
 app.use(internalErrorHandler);
 app.use(invalidRouteHandler);
