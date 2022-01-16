0cae055b7e49c092b5dd9c50175c5b5fe65a78b4
Yizhen Liu <imliuyzh@outlook.com>
2022-01-16 11:35:26 -0800

    Add GitHub link to front end

--- client/src/components/Chart/EmptyChart/EmptyChart.js
+++ client/src/components/Chart/EmptyChart/EmptyChart.js
@@ -1,4 +1,30 @@
 import { EmptyState, EmptyStateBody, EmptyStateVariant, Title } from '@patternfly/react-core';
+import styled from '@emotion/styled';
+import { ReactComponent as GithubIcon } from '../../../assets/images/github.svg';
+
+const GitHubLinkContainerElement = styled.div`
+    #github-button {
+        align-items: center;
+        background-color: transparent;
+        color: #000000;
+        cursor: pointer;
+        display: flex;
+        font-family: RedHatText, Arial, sans-serif;
+        font-size: 14px;
+        gap: 5px;
+        justify-content: center;
+        margin-top: 24px;
+    }
+
+    #github-button:hover {
+        text-decoration: none;
+    }
+
+    #github-icon {
+        height: 24px;
+        width: 24px;
+    }
+`;
 
 export default function EmptyChart() {
     return (
@@ -11,6 +37,12 @@ export default function EmptyChart() {
                 update the statistics once every quarter. In order to get this data, I must make a request to UCI's Public Records Office (PRO).
                 Sometimes, you may not find the data for a particular course due to a variety of circumstances such as late grade submittal. If this
                 happens, please contact me through GitHub issues.
+
+                <GitHubLinkContainerElement>
+                    <a href="https://github.com/imliuyzh/AntCatalog" id="github-button" target="_blank" rel="noreferrer">
+                        <GithubIcon fill="#000000" id="github-icon" />
+                    </a>
+                </GitHubLinkContainerElement>
             </EmptyStateBody>
         </EmptyState>
     );
