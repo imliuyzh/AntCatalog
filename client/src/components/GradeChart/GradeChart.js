f64033486e5d7206ae1e5fef398eda8afd46b2bb
Yizhen Liu <imliuyzh@outlook.com>
2022-02-06 11:33:22 -0800

    Fix indentation

--- client/src/components/GradeChart/GradeChart.js
+++ client/src/components/GradeChart/GradeChart.js
@@ -6,7 +6,7 @@ import { useSelector } from 'react-redux';
 let GradeChart = React.memo(() => {
     let searchResultState = useSelector(state => state.searchResult),
         selectedCoursesState = useSelector(state => state.selectedCourses);
-	
+
     const formatAggregateCoursesData = () => {
         let result = [
             {
@@ -55,101 +55,101 @@ let GradeChart = React.memo(() => {
         return result;
     };
 
-	const formatSelectedCoursesData = () => {
-		let result = [];
-		for (let course in selectedCoursesState) {
-			result.push({
-				name: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term}/${selectedCoursesState[course].courseCode})`,
-				grade: 'A',
-				title: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term})`,
-				courseCode: selectedCoursesState[course].courseCode,
-				amount: selectedCoursesState[course].gradeACount,
-				instructors: selectedCoursesState[course].instructors.join(`/`),
-				avgGPA: selectedCoursesState[course].gpaAvg
-			});
-			result.push({
-				name: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term}/${selectedCoursesState[course].courseCode})`,
-				grade: 'B',
-				title: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term})`,
-				courseCode: selectedCoursesState[course].courseCode,
-				amount: selectedCoursesState[course].gradeBCount,
-				instructors: selectedCoursesState[course].instructors.join(`/`),
-				avgGPA: selectedCoursesState[course].gpaAvg
-			});
-			result.push({
-				name: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term}/${selectedCoursesState[course].courseCode})`,
-				grade: 'C',
-				title: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term})`,
-				courseCode: selectedCoursesState[course].courseCode,
-				amount: selectedCoursesState[course].gradeCCount,
-				instructors: selectedCoursesState[course].instructors.join(`/`),
-				avgGPA: selectedCoursesState[course].gpaAvg
-			});
-			result.push({
-				name: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term}/${selectedCoursesState[course].courseCode})`,
-				grade: 'D',
-				title: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term})`,
-				courseCode: selectedCoursesState[course].courseCode,
-				amount: selectedCoursesState[course].gradeDCount,
-				instructors: selectedCoursesState[course].instructors.join(`/`),
-				avgGPA: selectedCoursesState[course].gpaAvg
-			});
-			result.push({
-				name: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term}/${selectedCoursesState[course].courseCode})`,
-				grade: 'F',
-				title: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term})`,
-				courseCode: selectedCoursesState[course].courseCode,
-				amount: selectedCoursesState[course].gradeFCount,
-				instructors: selectedCoursesState[course].instructors.join(`/`),
-				avgGPA: selectedCoursesState[course].gpaAvg
-			});
-			result.push({
-				name: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term}/${selectedCoursesState[course].courseCode})`,
-				grade: 'P',
-				title: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term})`,
-				courseCode: selectedCoursesState[course].courseCode,
-				amount: selectedCoursesState[course].gradePCount,
-				instructors: selectedCoursesState[course].instructors.join(`/`),
-				avgGPA: selectedCoursesState[course].gpaAvg
-			});
-			result.push({
-				name: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term}/${selectedCoursesState[course].courseCode})`,
-				grade: 'NP',
-				title: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term})`,
-				courseCode: selectedCoursesState[course].courseCode,
-				amount: selectedCoursesState[course].gradeNpCount,
-				instructors: selectedCoursesState[course].instructors.join(`/`),
-				avgGPA: selectedCoursesState[course].gpaAvg
-			});
-		}
-		return result;
-	};
+    const formatSelectedCoursesData = () => {
+        let result = [];
+        for (let course in selectedCoursesState) {
+            result.push({
+                name: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term}/${selectedCoursesState[course].courseCode})`,
+                grade: 'A',
+                title: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term})`,
+                courseCode: selectedCoursesState[course].courseCode,
+                amount: selectedCoursesState[course].gradeACount,
+                instructors: selectedCoursesState[course].instructors.join(`/`),
+                avgGPA: selectedCoursesState[course].gpaAvg
+            });
+            result.push({
+                name: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term}/${selectedCoursesState[course].courseCode})`,
+                grade: 'B',
+                title: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term})`,
+                courseCode: selectedCoursesState[course].courseCode,
+                amount: selectedCoursesState[course].gradeBCount,
+                instructors: selectedCoursesState[course].instructors.join(`/`),
+                avgGPA: selectedCoursesState[course].gpaAvg
+            });
+            result.push({
+                name: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term}/${selectedCoursesState[course].courseCode})`,
+                grade: 'C',
+                title: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term})`,
+                courseCode: selectedCoursesState[course].courseCode,
+                amount: selectedCoursesState[course].gradeCCount,
+                instructors: selectedCoursesState[course].instructors.join(`/`),
+                avgGPA: selectedCoursesState[course].gpaAvg
+            });
+            result.push({
+                name: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term}/${selectedCoursesState[course].courseCode})`,
+                grade: 'D',
+                title: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term})`,
+                courseCode: selectedCoursesState[course].courseCode,
+                amount: selectedCoursesState[course].gradeDCount,
+                instructors: selectedCoursesState[course].instructors.join(`/`),
+                avgGPA: selectedCoursesState[course].gpaAvg
+            });
+            result.push({
+                name: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term}/${selectedCoursesState[course].courseCode})`,
+                grade: 'F',
+                title: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term})`,
+                courseCode: selectedCoursesState[course].courseCode,
+                amount: selectedCoursesState[course].gradeFCount,
+                instructors: selectedCoursesState[course].instructors.join(`/`),
+                avgGPA: selectedCoursesState[course].gpaAvg
+            });
+            result.push({
+                name: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term}/${selectedCoursesState[course].courseCode})`,
+                grade: 'P',
+                title: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term})`,
+                courseCode: selectedCoursesState[course].courseCode,
+                amount: selectedCoursesState[course].gradePCount,
+                instructors: selectedCoursesState[course].instructors.join(`/`),
+                avgGPA: selectedCoursesState[course].gpaAvg
+            });
+            result.push({
+                name: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term}/${selectedCoursesState[course].courseCode})`,
+                grade: 'NP',
+                title: `${selectedCoursesState[course].department} ${selectedCoursesState[course].courseNumber} (${selectedCoursesState[course].term})`,
+                courseCode: selectedCoursesState[course].courseCode,
+                amount: selectedCoursesState[course].gradeNpCount,
+                instructors: selectedCoursesState[course].instructors.join(`/`),
+                avgGPA: selectedCoursesState[course].gpaAvg
+            });
+        }
+        return result;
+    };
 
-	const config = {
-		autoHide: true,
-		color: ['#0066cc', '#8bc1f7', '#002f5d', '#519de9', '#004b95'],
-		data: (searchResultState.isAggregateData) ? formatAggregateCoursesData() : formatSelectedCoursesData(),
-		isGroup: true,
-		label: {
-			autoHide: true,
-			layout: [{ type: 'interval-adjust-position' }, { type: 'interval-hide-overlap' }, { type: 'adjust-color' }],
-		},
-		legend: (searchResultState.isAggregateData)
-			? false
-			: {
-				layout: 'horizontal',
-				position: 'bottom',
-			},
-		locale: 'en-US',
-		marginRatio: 0,
-		padding: 'auto',
-		seriesField: 'name',
-		tooltip: { customContent: (title, items) => <CourseTooltip isAggregateData={searchResultState.isAggregateData} items={items} title={title} />},
-		xField: 'grade',
-		yField: 'amount',
-	};
+    const config = {
+        autoHide: true,
+        color: ['#0066cc', '#8bc1f7', '#002f5d', '#519de9', '#004b95'],
+        data: (searchResultState.isAggregateData) ? formatAggregateCoursesData() : formatSelectedCoursesData(),
+        isGroup: true,
+        label: {
+            autoHide: true,
+            layout: [{ type: 'interval-adjust-position' }, { type: 'interval-hide-overlap' }, { type: 'adjust-color' }],
+        },
+        legend: (searchResultState.isAggregateData)
+            ? false
+            : {
+                layout: 'horizontal',
+                position: 'bottom',
+            },
+        locale: 'en-US',
+        marginRatio: 0,
+        padding: 'auto',
+        seriesField: 'name',
+        tooltip: { customContent: (title, items) => <CourseTooltip isAggregateData={searchResultState.isAggregateData} items={items} title={title} />},
+        xField: 'grade',
+        yField: 'amount',
+    };
 
-	return <Column {...config} />;
+    return <Column {...config} />;
 });
 
 export default GradeChart;
