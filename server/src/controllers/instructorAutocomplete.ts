cfa7276f6a8eb749bf7de086f9578b37860f91b2
Yizhen Liu <imliuyzh@outlook.com>
2022-02-22 16:39:54 -0800

    Remove global variable usage

--- server/src/controllers/instructorAutocomplete.ts
+++ server/src/controllers/instructorAutocomplete.ts
@@ -7,7 +7,29 @@ import { Result, ValidationError, validationResult } from 'express-validator';
 import logger from '../utils/logger';
 import sequelize from '../db/sequelize';
 
-const cache: NodeCache = new NodeCache();
+/**
+ * A namespace storing a cache of a list of all instructors, intended to be used only in this file
+ */
+namespace CacheNamespace {
+    let cache: NodeCache = new NodeCache();
+
+    /**
+     * Return a list of all instructors in the database -
+     * if it is not in the cache, load it from the database and store it in the cache
+     * @returns a promise containing every instructor's name
+     */
+    export async function loadInstructors(): Promise<string[]> {
+        let instructorList: string[] = cache.get('instructors') ?? [];
+        if (instructorList.length <= 0) {
+            logger.info(`Begin to Retrieve All the Instructors' Name...`);
+            let instructors: Array<{ name: string }> = await sequelize.query('SELECT DISTINCT name FROM Instructor', { type: QueryTypes.SELECT });
+            instructors.forEach((instructor: { name: string }) => instructorList.push(instructor['name']));
+            cache.set('instructors', instructorList);
+            logger.info(`Finished Retrieving All the Instructors' Name.`);
+        }
+        return instructorList;
+    }
+}
 
 /**
  * A controller for instructor fuzzy search.
@@ -30,7 +52,7 @@ export default async function InstructorAutocompleteController(req: express.Requ
             return;
         }
 
-        let instructorList: string[] = await loadInstructors();
+        let instructorList: string[] = await CacheNamespace.loadInstructors();
         let fuse: Fuse<string> = new Fuse(instructorList, { minMatchCharLength: 3 });
         let matches: string[] = (req.query.name !== undefined) ? fuse.search(req.query.name.toString(), { limit: 5 }).map(match => match.item) : [];
         logger.info(`${req.ip} ${req.method} ${req.originalUrl} ${JSON.stringify(matches)}`);
@@ -38,21 +60,4 @@ export default async function InstructorAutocompleteController(req: express.Requ
     } catch (exception: any) {
         next(exception);
     }
-}
-
-/**
- * Return a list of all instructors in the database -
- * if it is not in the cache, load it from the database and store it in the cache
- * @returns a promise containing every instructor's name
- */
-async function loadInstructors(): Promise<string[]> {
-    let instructorList: string[] = cache.get('instructors') ?? [];
-    if (instructorList.length <= 0) {
-        logger.info(`Begin to Retrieve All the Instructors' Name...`);
-        let instructors: Array<{ name: string }> = await sequelize.query('SELECT DISTINCT name FROM Instructor', { type: QueryTypes.SELECT });
-        instructors.forEach((instructor: { name: string }) => instructorList.push(instructor['name']));
-        cache.set('instructors', instructorList);
-        logger.info(`Finished Retrieving All the Instructors' Name.`);
-    }
-    return instructorList;
 }
