fbb640ab9fface9e0d258414cc151ee44b33f023
Yizhen Liu <imliuyzh@outlook.com>
2022-07-01 22:57:50 -0700

    Prepare for Azure deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import logger from './utils/logger';
 
-const PORT: number = 26997;
+const PORT: number = (process.env.PORT !== undefined) ? parseInt(process.env.PORT) : 26997;
 
 const server = app.listen(PORT, () => logger.info(`Server is Running on Port #${PORT}.`));
 
