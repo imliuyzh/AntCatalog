70ce877b2c958fc75d624f2a482fdb4f4901382b
Yizhen Liu <imliuyzh@outlook.com>
2022-07-27 15:54:31 -0700

    Add tooltips for course code and course number fields

--- client/src/components/SearchForm/CourseCodeInput/CourseCodeInput.js
+++ client/src/components/SearchForm/CourseCodeInput/CourseCodeInput.js
@@ -1,18 +1,21 @@
 import CreatableSelect from 'react-select/creatable';
 import { styles2 } from '../../../utils/searchFormStyles';
+import { Tooltip } from '@patternfly/react-core';
 import { updateFormInput } from '../../../features/internalStateSlice';
 import { useDispatch } from 'react-redux';
 
 export default function CourseCodeInput({ inputRef }) {
     let internalStateDispatch = useDispatch();
     return (
-        <CreatableSelect
-            aria-label="course-code-input"
-            isMulti
-            onChange={(newValue, _) => internalStateDispatch(updateFormInput({ courseCode: newValue.map(({ value }) => value) }))}
-            placeholder="Course Codes"
-            ref={inputRef}
-            styles={styles2}
-        />
+        <Tooltip content="Please provide 5-digit codes for classes (e.g. 14200, 29000 & 29100).">
+            <CreatableSelect
+                aria-label="course-code-input"
+                isMulti
+                onChange={(newValue, _) => internalStateDispatch(updateFormInput({ courseCode: newValue.map(({ value }) => value) }))}
+                placeholder="Course Codes"
+                ref={inputRef}
+                styles={styles2}
+            />
+        </Tooltip>
     );
 }
