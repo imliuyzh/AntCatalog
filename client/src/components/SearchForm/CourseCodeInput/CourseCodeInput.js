10807c7806e7b9e17a04da0adab050fd61ab9180
Yizhen Liu <imliuyzh@outlook.com>
2022-07-27 18:22:04 -0700

    Show tooltip on mobile

--- client/src/components/SearchForm/CourseCodeInput/CourseCodeInput.js
+++ client/src/components/SearchForm/CourseCodeInput/CourseCodeInput.js
@@ -3,15 +3,25 @@ import { styles2 } from '../../../utils/searchFormStyles';
 import { Tooltip } from '@patternfly/react-core';
 import { updateFormInput } from '../../../features/internalStateSlice';
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 export default function CourseCodeInput({ inputRef }) {
     let internalStateDispatch = useDispatch();
+    let [isTooltipVisible, setIsTooltipVisible] = useState(false);
+
     return (
-        <Tooltip content="Please provide 5-digit codes for classes (e.g. 14200, 29000 & 29100).">
+        <Tooltip
+            content="Please provide 5-digit codes for classes (e.g. 14200, 29000 & 29100)."
+            isContentLeftAligned
+            isVisible={isTooltipVisible}
+            trigger="manual"
+        >
             <CreatableSelect
                 aria-label="course-code-input"
                 isMulti
+                onBlur={(_) => setIsTooltipVisible(false)}
                 onChange={(newValue, _) => internalStateDispatch(updateFormInput({ courseCode: newValue.map(({ value }) => value) }))}
+                onFocus={(_) => setIsTooltipVisible(true)}
                 placeholder="Course Codes"
                 ref={inputRef}
                 styles={styles2}
--- client/src/components/SearchForm/CourseNumberInput/CourseNumberInput.js
+++ client/src/components/SearchForm/CourseNumberInput/CourseNumberInput.js
@@ -3,15 +3,25 @@ import { styles2 } from '../../../utils/searchFormStyles';
 import { Tooltip } from '@patternfly/react-core';
 import { updateFormInput } from '../../../features/internalStateSlice';
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 export default function CourseNumberInput({ inputRef }) {
     let internalStateDispatch = useDispatch();
+    let [isTooltipVisible, setIsTooltipVisible] = useState(false);
+
     return (
-        <Tooltip content="Please provide alphanumeric codes for courses (e.g. H2A, 8C & 102).">
+        <Tooltip
+            content="Please provide alphanumeric codes for courses (e.g. H2A, 8C & 102)."
+            isContentLeftAligned
+            isVisible={isTooltipVisible}
+            trigger="manual"
+        >
             <CreatableSelect
                 aria-label="course-number-input"
                 isMulti
+                onBlur={(_) => setIsTooltipVisible(false)}
                 onChange={(newValue, _) => internalStateDispatch(updateFormInput({ courseNumber: newValue.map(({ value }) => value) }))}
+                onFocus={(_) => setIsTooltipVisible(true)}
                 placeholder="Course Numbers"
                 ref={inputRef}
                 styles={styles2}
