c685b0f7d3d6f5ee95c4cdfc0ad8c9bdd1f32fe5
Yizhen Liu <imliuyzh@outlook.com>
2022-07-27 19:44:37 -0700

    Change department placeholder string

--- client/src/components/SearchForm/DepartmentMenu/DepartmentMenu.js
+++ client/src/components/SearchForm/DepartmentMenu/DepartmentMenu.js
@@ -619,7 +619,7 @@ export default function DepartmentMenu({ inputRef }) {
             isMulti
             onChange={(option) => internalStateDispatch(updateFormInput({ department: option.map(({ value }) => value) }))}
             options={DEPARTMENT}
-            placeholder="All Departments"
+            placeholder="Include All Departments"
             ref={inputRef}
             styles={styles1}
         />
