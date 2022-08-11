1f8edc622f43eae3a32f4b535f0c8e766695981c
Yizhen Liu <imliuyzh@outlook.com>
2022-08-10 23:01:21 -0700

    Enable dev tools only in development mode

--- client/src/store.js
+++ client/src/store.js
@@ -4,6 +4,7 @@ import searchResultReducer from './features/searchResultSlice';
 import selectedCoursesReducer from './features/selectedCoursesSlice';
 
 export const store = configureStore({
+    devTools: process.env.NODE_ENV !== 'production',
     reducer: {
         internalState: internalStateReducer,
         searchResult: searchResultReducer,
