a9ee9e8c13df284ea8d4cdfc657e53572252550f
Yizhen Liu <imliuyzh@outlook.com>
2022-09-11 16:51:46 -0700

    Revise error page design

--- client/src/pages/ErrorPage/ErrorPage.js
+++ client/src/pages/ErrorPage/ErrorPage.js
@@ -1,40 +1,27 @@
+import { Card, CardBody, CardFooter, CardTitle } from '@patternfly/react-core';
 import ErrorIllustration from '../../assets/images/error-bg.jpg';
 import { Link } from 'react-router-dom';
 import styled from '@emotion/styled';
-import { Title } from '@patternfly/react-core';
+import { Text, TextContent, TextVariants } from '@patternfly/react-core';
 import { useEffect } from 'react';
 
-const ErrorPageContainerElement = styled.div`
+const ErrorPageContainerElement = styled.main`
+    background-image: url(${ErrorIllustration});
+    background-size: cover;
+    background-repeat: no-repeat;
+    display: grid;
+    height: 100vh;
+    padding: 2rem;
+    place-items: center;
+    width: 100vw;
+
     a, a:hover {
         text-decoration: none;
-    }
-    
-    #figure {
-        line-height: 0;
-        margin: 0;
-    }
-
-    #content {
-        align-items: center;
-        display: flex;
-        flex-direction: column;
-        height: 512px;
-        justify-content: center;
-        text-align: center;
-    }
-    
-    #title {
-        font-size: 4rem;
-    }
-
-    #message {
-        padding: 0 36px;
     }
 
     #description {
         color: #6a6e73;
         line-height: 2rem;
-        margin: 1rem;
     }
     
     #home-button {
@@ -47,16 +34,7 @@ const ErrorPageContainerElement = styled.div`
         font-size: 14px;
         gap: 5px;
         justify-content: center;
-        margin: 6rem 0 0;
-        padding: 8px 28px;
-        transition: all 0.5s;
-        width: 100%;
-    }
-    
-    #home-button:hover {
-        background-color: transparent;
-        color: rgb(170, 179, 188);
-        text-decoration: none;
+        padding: 8px 20px;
     }
 `;
 
@@ -64,20 +42,22 @@ export default function ErrorPage() {
     useEffect(() => document.title = 'Error | AntCatalog', []);
     return (
         <ErrorPageContainerElement>
-            <figure id="figure">
-                <img src={ErrorIllustration} alt="" />
-            </figure>
-
-            <main id="content">
-                <div id="message">
-                    <Title headingLevel="h1" id="title">Error</Title>
-                    <p id="description">Apologies, the page you're looking for cannot be found.</p>
-                </div>
-                
-                <Link to="/">
-                    <button id="home-button">Return to Home</button>  
-                </Link>
-            </main>
+            <Card isLarge>
+                <CardTitle>Error</CardTitle>
+                <CardBody>
+                    <TextContent>
+                        <Text component={TextVariants.small}>
+                            Apologies, the page you're looking for cannot be found.
+                            Please go back to the home page using the button below.
+                        </Text>
+                    </TextContent>
+                </CardBody>
+                <CardFooter>
+                    <Link to="/">
+                        <button id="home-button">Return to Home</button>  
+                    </Link>
+                </CardFooter>
+            </Card>
         </ErrorPageContainerElement>
     );
 }
