339f95b411abc7827d0304e93a058086f54375ff
Yizhen Liu <imliuyzh@outlook.com>
2022-09-16 16:00:41 +0000

    Eliminate extra async keyword

--- server/src/__tests__/staticFilesPathSetter.test.ts
+++ server/src/__tests__/staticFilesPathSetter.test.ts
@@ -6,30 +6,30 @@ beforeAll(() => {
 });
 
 describe('test determineIndexFilePath()', () => {
-    test('should respond with correct path when PRODUCTION_ENV is aws', async () => {
+    test('should respond with correct path when PRODUCTION_ENV is aws', () => {
         process.env.PRODUCTION_ENV = 'aws';
         expect(determineIndexFilePath()).toContain('client/build/index.html');
     });
-    test('should respond with correct path when PRODUCTION_ENV is azure', async () => {
+    test('should respond with correct path when PRODUCTION_ENV is azure', () => {
         process.env.PRODUCTION_ENV = 'azure';
         expect(determineIndexFilePath()).toContain('public/index.html');
     });
-    test('should throw exception when PRODUCTION_ENV is something else', async () => {
+    test('should throw exception when PRODUCTION_ENV is something else', () => {
         process.env.PRODUCTION_ENV = '';
         expect(determineIndexFilePath).toThrow();
     });
 });
 
 describe('test determineStaticFileFolder()', () => {
-    test('should respond with correct folder when PRODUCTION_ENV is aws', async () => {
+    test('should respond with correct folder when PRODUCTION_ENV is aws', () => {
         process.env.PRODUCTION_ENV = 'aws';
         expect(determineStaticFileFolder()).toContain('client/build');
     });
-    test('should respond with correct folder when PRODUCTION_ENV is azure', async () => {
+    test('should respond with correct folder when PRODUCTION_ENV is azure', () => {
         process.env.PRODUCTION_ENV = 'azure';
         expect(determineStaticFileFolder()).toContain('public');
     });
-    test('should throw exception when PRODUCTION_ENV is something else', async () => {
+    test('should throw exception when PRODUCTION_ENV is something else', () => {
         process.env.PRODUCTION_ENV = '';
         expect(determineStaticFileFolder).toThrow();
     });
