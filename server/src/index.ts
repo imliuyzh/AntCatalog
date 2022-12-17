cd998fc727601ca0860397a5cbfab4fa470e200d
Yizhen Liu <imliuyzh@outlook.com>
2022-12-17 11:52:06 -0800

    Use single quotation mark

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 import logger from './utils/logger';
 
-const port: number = parseInt(process.env.PORT ?? "26997");
+const port: number = parseInt(process.env.PORT ?? '26997');
 
 app.listen(port, () => logger.info(`Server is Running on Port #${port}.`));
