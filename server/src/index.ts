ae3cc06ecf81cee8a2dfb2b31c685262c2089c32
Yizhen Liu <imliuyzh@outlook.com>
2022-12-17 11:03:15 -0800

    Remove unneeded code

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,4 @@ import logger from './utils/logger';
 
 const PORT: number = (process.env.PORT !== undefined) ? parseInt(process.env.PORT) : 26997;
 
-const server = app.listen(PORT, () => logger.info(`Server is Running on Port #${PORT}.`));
-
-process.on('SIGTERM', () => server.close(() => logger.info(`Server is Closed.`)));
+app.listen(PORT, () => logger.info(`Server is Running on Port #${PORT}.`));
