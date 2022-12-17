7b3c15bdf1c92fe5f96c9daff090b18e368c2498
Yizhen Liu <imliuyzh@outlook.com>
2022-12-17 11:44:56 -0800

    Shorten the condition for server port

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 import logger from './utils/logger';
 
-const PORT: number = (process.env.PORT !== undefined) ? parseInt(process.env.PORT) : 26997;
+const port: number = parseInt(process.env.PORT ?? "26997");
 
-app.listen(PORT, () => logger.info(`Server is Running on Port #${PORT}.`));
+app.listen(port, () => logger.info(`Server is Running on Port #${port}.`));
